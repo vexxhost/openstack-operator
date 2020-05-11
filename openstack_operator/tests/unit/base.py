0a3945eac2ac12121a694211d55ba304fb431dec
okozachenko <okozachenko@vexxhost.com>
2020-05-12 00:09:34 +0300

    Fix get_secret utils func to filter for a specific namespace
    Change-Id: I7242f0a2d617ebe38b9b46acb69726d7f708046e

--- openstack_operator/tests/unit/base.py
+++ openstack_operator/tests/unit/base.py
@@ -34,6 +34,7 @@ class KubernetesObjectTestCase(testtools.TestCase):
     SAMPLES_PATH = 'config/samples'
     SAMPLE_FILE = ''
     TEMPLATE_FILE = ''
+
     @classmethod
     def setUpClass(cls):
         sample_path = "%s/%s" % (cls.SAMPLES_PATH, cls.SAMPLE_FILE)
