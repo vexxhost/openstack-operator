fb6fde762d3ddfc1ab9251fcb145878d941dcdec
okozachenko <okozachenko@vexxhost.com>
2020-08-10 22:29:34 +0300

    Add applicationcredential to magnum
    Change-Id: Iae4b999ef765eb1e962772170915549fc5ad4aeb

--- openstack_operator/magnum.py
+++ openstack_operator/magnum.py
@@ -60,6 +60,9 @@ def create_or_resume(name, spec, **_):
                                name=name, spec=spec)
         url = spec["ingress"]["host"]
 
+    # Create application credential
+    identity.ensure_application_credential(name="magnum")
+
     # Create service and endpoints
     if "endpoint" not in spec:
         spec["endpoint"] = True
